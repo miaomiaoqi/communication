be4132c16ded3dcc2544c29487536fc131e9ee8b
miaoqi <miaoqi3@xdf.cn>
2024-01-15 18:32:59 +0800

    update blog

---
+++ atguigu-netty/src/main/java/com/atguigu/nio/NIOFileChannel01.java
@@ -0,0 +1,13 @@
+package com.atguigu.nio;
+
+import java.io.FileOutputStream;
+
+public class NIOFileChannel01 {
+
+    public static void main(String[] args){
+        String str = "hello, 尚硅谷";
+        // 创建一个输出流 -> channel
+        FileOutputStream fileOutputStream = new FileOutputStream("");
+    }
+
+}
